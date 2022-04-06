1bbc03212139acb7990201f187ce3957ea3e01c1
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2022-04-06 02:24:10 -0300

    Learning about the Truffle

--- Solidity/truffle/contracts/hello.sol
+++ Solidity/truffle/contracts/hello.sol
@@ -2,7 +2,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-    function hello() public pure returns (string memory) {
-        return "Hello, world!";
+    string message = "Hello World";
+
+    function setMessage(string memory newMessage) public payable {
+        message = newMessage;
+    }
+
+    function hello() public view returns (string memory) {
+        return message;
     }
 }
+
+/**
+    let instance = await HelloWorld.deployed()
+    instance.hello()
+    instance.setMessage("Hi!")
+    instance.hello()
+
+    setMessage.call.("") => Don't value the value of the blockchain only executes the function with the new value.
+    setMessage("") => I'll do a transaction and the value will be stored in the blockchain.
+    
+    value => We can pass a parameter with the Wei quantity.
+ */
